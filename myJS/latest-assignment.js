675b590a7ea41c7bce715be58b26e72eb5fc0ae0
HexkB0i <bbcamel@saintly.com>
2024-12-28 15:19:12 +0100

    fixed the linking weirdness that was resulting in fonts and scripts falling off. hope the hyperlinks still work after the changes to the directory structure.

--- myJS/latest-assignment.js
+++ myJS/latest-assignment.js
@@ -6,12 +6,6 @@ const lastAccordionItem = accordionItems[accordionItems.length - 1];
 const lastBody = lastAccordionItem.querySelector('.accordion-body');
 const bodyText = lastBody.innerText;
 
-// Log the results
-console.log('Body Text:', bodyText);
-buttons.forEach((button, index) => {
-  console.log(`Button ${index + 1}:`, button.textContent);
-});
-
 // Insert it
 document.getElementById("latest-assignment__description").innerHTML = bodyText;
 
