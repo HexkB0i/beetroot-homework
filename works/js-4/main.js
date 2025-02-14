dd1db2ffff99646313e2957519cb3f9bcab69e4f
HexkB0i <bbcamel@saintly.com>
2025-02-14 20:43:26 +0100

    started binary search homework

---
+++ works/js-4/main.js
@@ -0,0 +1,13 @@
+// Guessing the user's number
+
+let numbArr = Array.from(Array(10).keys());
+console.log(numbArr);
+
+let userNumb = +prompt('Enter a number between 0 and 100:');
+
+function guessNumber(){
+    let min = 0;
+    let max = 100;
+
+
+};
