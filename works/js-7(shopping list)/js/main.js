5fbef198ef034d5a4f97c7c08c9a2c768cc7ea0a
HexkB0i <bbcamel@saintly.com>
2025-03-05 23:50:14 +0100

    Minimum program done. Now to style the UI, add strikethrough and save to memory between refreshes.

---
+++ works/js-7(shopping list)/js/main.js
@@ -0,0 +1,56 @@
+const productList = [];
+
+// Builds a product object to insert into the list. 
+// id is recieved through assignId() function.
+function createProduct(name, amount, isBought, id){
+    let product = {};
+
+    product.name = name;
+    product.amount= amount;
+    product.isBought = isBought;
+    product.id = id;
+
+    return product;
+}
+
+function assignId(){
+    let id;
+
+    if (productList.length === 0){
+        id = 0;
+    }else{
+        id = productList.length - 1;
+    }
+
+    return id;
+};
+
+// Dom elements
+let nameInput = document.querySelector('#nameInput')
+let amountInput = document.querySelector('#amountInput')
+let isBoughtInput = document.querySelector('#isBoughtInput')
+
+let listField = document.querySelector('.products-list') 
+
+let btnAdd = document.querySelector('#btn-add')
+
+// Page actions
+function addProduct(){
+    let product = createProduct(nameInput.value, amountInput.value, isBoughtInput.checked, assignId());
+
+    productList.push(product);
+};
+
+function printProductList(){
+   for(let el of productList){
+        listField.innerHTML += `<li>${el.name} - x${el.amount} - ${el.isBought}</li>`
+    };
+};
+
+function btnAddAction(){
+    listField.innerHTML = ``;
+    addProduct();
+    printProductList();
+};
+
+btnAdd.addEventListener('click', btnAddAction)
